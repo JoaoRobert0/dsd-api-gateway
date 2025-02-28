2d3a32bc98c87ae055679f294a895ae62e493b19
JoaoRobert0 <devjoaoroberto@gmail.com>
2025-02-27 21:23:05 -0300

    :sparkles: feat: gateway redirecionando para serviço REST

--- gateway/main.py
+++ gateway/main.py
@@ -1,8 +1,37 @@
 from fastapi import FastAPI
 import requests
 import xml.etree.ElementTree as ET
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# URL do serviço de produtos
+PRODUCTS_SERVICE_URL = "http://localhost:3000/produtos"  # Supondo que o serviço de produtos esteja rodando na porta 3000
+
+# Endpoint para redirecionar requisições GET para o serviço de produtos
+@app.get("/produtos")
+def get_produtos():
+    # Realiza uma requisição GET para o serviço de produtos
+    response = requests.get(PRODUCTS_SERVICE_URL)
+
+    # Retorna a resposta do serviço de produtos
+    return response.json()
+
+# Modelo Pydantic para o Produto
+class Produto(BaseModel):
+    nome: str
+    preco: float
+
+# Endpoint para criar um novo produto
+@app.post("/produtos")
+def create_produto(produto: Produto):
+    # Converte o produto para dicionário (dict) antes de enviar
+    response = requests.post(PRODUCTS_SERVICE_URL, json=produto.dict())
+
+    # Retorna a resposta do serviço de produtos
+    return response.json()
+
+
 
 @app.post("/soma")
 def soap_add(arg0: int, arg1: int):
